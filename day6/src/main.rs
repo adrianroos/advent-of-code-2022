572fd728d9017aa94ce332e5c09c5c1e4421945a
Adrian Roos <roosa@google.com>
2022-12-06 21:30:43 +0100

    day6 linear

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::{io::{self}};
+use std::io::{self};
 
 fn main() {
     let mut line = String::new();
@@ -7,46 +7,87 @@ fn main() {
 
     let packet_start = find_start(line.as_bytes(), 4);
     let message_start = find_start(line.as_bytes(), 14);
-    
+
     println!("packet: {packet_start} message: {message_start}");
 }
 
 fn find_start(input: &[u8], size: usize) -> usize {
-    for i in size..input.len() {
-        if distinct(&input[i-size .. i]) {
-            return i
+    let mut unique = 0;
+    let mut counts = [0usize; u8::MAX as usize + 1];
+    for i in 0..input.len() {
+        {
+            let x = input[i];
+            let x_count = &mut counts[x as usize];
+            if *x_count == 0 {
+                unique += 1;
+            }
+            *x_count += 1;
+        }
+
+        if i >= size {
+            let y = input[i - size];
+            let y_count = &mut counts[y as usize];
+            *y_count -= 1;
+            if *y_count == 0 {
+                unique -= 1;
+            }
+        }
+
+        if unique == size {
+            return i + 1;
         }
     }
     panic!("no packet start found")
 }
 
-fn distinct(input: &[u8]) -> bool {
-    let mut counts = [0usize; u8::MAX as usize + 1];
-    for &x in input {
-        counts[x as usize] += 1;
-    }
-    counts.iter().all(|&x| x <= 1)
-}
-
 #[cfg(test)]
 mod test {
     use super::find_start;
-    
+
     #[test]
     fn sample_part1() {
-        assert_eq!(find_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb".as_bytes(), 4), 7);
+        assert_eq!(
+            find_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb".as_bytes(), 4),
+            7
+        );
         assert_eq!(find_start("bvwbjplbgvbhsrlpgdmjqwftvncz".as_bytes(), 4), 5);
         assert_eq!(find_start("nppdvjthqldpwncqszvftbrmjlhg".as_bytes(), 4), 6);
-        assert_eq!(find_start("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".as_bytes(), 4), 10);
-        assert_eq!(find_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes(), 4), 11);
+        assert_eq!(
+            find_start("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".as_bytes(), 4),
+            10
+        );
+        assert_eq!(
+            find_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes(), 4),
+            11
+        );
     }
 
     #[test]
     fn sample_part2() {
-        assert_eq!(find_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb".as_bytes(), 14), 19);
-        assert_eq!(find_start("bvwbjplbgvbhsrlpgdmjqwftvncz".as_bytes(), 14), 23);
-        assert_eq!(find_start("nppdvjthqldpwncqszvftbrmjlhg".as_bytes(), 14), 23);
-        assert_eq!(find_start("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".as_bytes(), 14), 29);
-        assert_eq!(find_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes(), 14), 26);
+        assert_eq!(
+            find_start("mjqjpqmgbljsphdztnvjfqwrcgsmlb".as_bytes(), 14),
+            19
+        );
+        assert_eq!(
+            find_start("bvwbjplbgvbhsrlpgdmjqwftvncz".as_bytes(), 14),
+            23
+        );
+        assert_eq!(
+            find_start("nppdvjthqldpwncqszvftbrmjlhg".as_bytes(), 14),
+            23
+        );
+        assert_eq!(
+            find_start("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".as_bytes(), 14),
+            29
+        );
+        assert_eq!(
+            find_start("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes(), 14),
+            26
+        );
+    }
+
+    #[test]
+    fn other() {
+        assert_eq!(find_start("abcd".as_bytes(), 4), 4);
     }
 }
